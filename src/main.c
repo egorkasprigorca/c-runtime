df3a91a154288bc349ad6cfb9f32fc698a59c5de
egorkasprigorca <chief.bolotow2013@gmail.com>
2025-03-30 20:11:39 +0700

    refactor code

--- src/main.c
+++ src/main.c
@@ -14,155 +14,110 @@
 #include <sys/wait.h>
 #include <sys/mount.h>
 #include <sys/stat.h>
+#include <linux/prctl.h>
+#include <sys/prctl.h>
 
-static int sys_clone(unsigned long flags, void *child_stack) {
-    return (int)syscall(SYS_clone, flags, child_stack);
-}
-
-static int pivot_root(const char *new_root, const char *put_old) {
+static int
+pivot_root(const char *new_root, const char *put_old)
+{
     return syscall(SYS_pivot_root, new_root, put_old);
 }
 
 typedef struct {
     uint8_t detach;
+    int reader_pipe;
     char *command;
     char **command_args;
-} container_run_opts;
+} container_opts_t;
 
-int set_usernamespace(const pid_t pid, const uid_t host_uid, const gid_t host_gid) {
-    char path[64];
+#define STACKSIZE (1024*1024)
+static char container_stack[STACKSIZE];
+
+int setup_userns(const pid_t pid, const int uid, const int gid) {
+    printf("Preparing user namespace\n");
     FILE *file;
+    char path[64];
 
     sprintf(path, "/proc/%d/uid_map", pid);
     file = fopen(path, "w");
-    fprintf(file, "0 %d 1\n", host_uid);
+    if (file == NULL)
+        err(EXIT_FAILURE, "Couldn't open uid_map");
+    fprintf(file, "0 %d 1\n", uid);
     fclose(file);
 
     sprintf(path, "/proc/%d/setgroups", pid);
     file = fopen(path, "w");
+    if (file == NULL)
+        err(EXIT_FAILURE, "Couldn't open setgroups");
     fprintf(file, "deny");
     fclose(file);
 
     sprintf(path, "/proc/%d/gid_map", pid);
     file = fopen(path, "w");
-    fprintf(file, "0 %d 1\n", host_gid);
+    if (file == NULL)
+        err(EXIT_FAILURE, "Couldn't open gid_map");
+    fprintf(file, "0 %d 1\n", gid);
     fclose(file);
 
-    printf("setted up usernamespace");
-
     return 0;
 }
 
-int set_rootfs() {
-    int ret;
-    char buf[128];
-    printf("Effective UID: %d\n", geteuid());
-
-    chdir("..");
-    ret = mount("root_fs", "root_fs", "", MS_BIND, "");
-    if (ret < 0)
-        err(EXIT_FAILURE, "mount");
-
-    printf("Current directory: %s\n", getcwd(buf, sizeof(buf)));
-    ret = mkdir("root_fs/old_root", 0777);
-    if (ret < 0 && errno != EEXIST)
-        err(EXIT_FAILURE, "mkdir old_root");
-
-    ret = pivot_root(".", "root_fs/old_root");
-    if (ret < 0)
-        err(EXIT_FAILURE, "pivot_root");
-
-    ret = umount2("old_root", MNT_DETACH);
-    if (ret < 0)
-        err(EXIT_FAILURE, "umount2");
-
+int setup_rootfs(const char *bundle_path) {
+    printf("Setting up rootfs\n");
+    const char *oldrootfs_path = ".oldrootfs";
+    const char *rootfs_path = "rootfs";
+    int mount_flags = MS_BIND;
+    const char *mount_name = "rootfs";
+    chdir(bundle_path);
+    struct stat st;
+    if (stat(rootfs_path, &st) == -1 || !S_ISDIR(st.st_mode))
+        err(EXIT_FAILURE, "RootFS directory '%s' does not exist", rootfs_path);
+    if (mount(mount_name, rootfs_path, "ext4", mount_flags, "") == -1)
+        err(EXIT_FAILURE, "Failed to mount rootfs");
+    chdir(rootfs_path);
+    if (mkdir(oldrootfs_path, 0777) == -1 && errno != EEXIST)
+        err(EXIT_FAILURE, "Failed to create oldrootfs directory");
+    if (pivot_root(".", oldrootfs_path) == -1)
+        err(EXIT_FAILURE, "Failed to pivot root");
+    if (chdir("/") == -1)
+        err(EXIT_FAILURE, "Failed to chroot /");
+    if (umount2(oldrootfs_path, MNT_DETACH) == -1)
+        err(EXIT_FAILURE, "Failed to umount rootfs");
     return 0;
 }
 
-pid_t container_run(container_run_opts *run_opts, const uid_t host_uid, const gid_t host_gid) {
-    pid_t pid;
-    int unshare_flags = CLONE_NEWUSER | CLONE_NEWUTS | CLONE_NEWNS | SIGCHLD;
-    pid = sys_clone(unshare_flags, NULL);
-    if (pid < 0)
-        err(EXIT_FAILURE, "clone");
-
-    if (pid > 0)
-        return pid;
-
-    if (run_opts->detach && setsid() < 0)
-        err(EXIT_FAILURE, "setsid");
-
-    printf("Child process: PID = %d\n", getpid());
-    printf("Setting up root filesystem...\n");
-    set_rootfs();
-
-    if (execvp(run_opts->command, run_opts->command_args) < 0)
-        _exit(EXIT_FAILURE);
-
-    return 0;
-}
-
-int detach_process() {
-    pid_t pid;
-    if (setsid() < 0)
-        return -1;
-    pid = fork();
-    if (pid < 0)
-        return -1;
-    if (pid != 0)
-        _exit(EXIT_SUCCESS);
-    fclose(stdin);
-    fclose(stdout);
-    fclose(stderr);
+int container_run(void *arg) {
+    prctl(PR_SET_PDEATHSIG, SIGKILL);
+    char buf[2];
+    container_opts_t *run_opts = (container_opts_t*)arg;
+    int n = read(run_opts->reader_pipe, buf, 2);
+    setup_rootfs("../bundle");
+    int res = execvp(run_opts->command, run_opts->command_args);
+    if (res == -1)
+        err(EXIT_FAILURE, "Failed to execute container process command");
     return 0;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <command> [args...]\n", argv[0]);
-        return EXIT_FAILURE;
+    int fds[2];
+    if (pipe(fds) == -1) {
+        err(EXIT_FAILURE, "Failed to create communication pipe");
     }
-
-    container_run_opts run_opts = {
+    container_opts_t opts = {
         .detach = 0,
         .command = argv[1],
-        .command_args = argv + 1
+        .command_args = &argv[1],
+        .reader_pipe = fds[0]
     };
-
-    uid_t host_uid = getuid();
-    uid_t host_gid = getgid();
-    int ret;
-
-    if (run_opts.detach) {
-        ret = fork();
-        if (ret)
-            return 0;
-        ret = detach_process();
-        if (ret < 0)
-            err(EXIT_FAILURE, "detach process");
-    }
-
-    pid_t pid = container_run(&run_opts, host_uid, host_gid);
-    set_usernamespace(pid, host_uid, host_gid);
-
-    if (run_opts.detach) {
-        FILE *pid_file = fopen("../mydaemon.pid", "w");
-        if (pid_file != NULL) {
-            fprintf(pid_file, "%d\n", pid);
-            fclose(pid_file);
-        }
-        return 0;
-    }
-
-    while (1) {
-        int status;
-        int r = waitpid(pid, &status, 0);
-        if (r < 0) {
-            if (errno == EINTR)
-                continue;
-            return EXIT_FAILURE;
-        }
-        if (WIFEXITED(status) || WIFSIGNALED(status))
-            return WEXITSTATUS(status);
-    }
+    int flags = CLONE_NEWUSER | CLONE_NEWUTS | CLONE_NEWNS | SIGCHLD;
+    pid_t pid = clone(container_run, container_stack + STACKSIZE, flags, &opts);
+    setup_userns(pid, getuid(), getgid());
+    int n = write(fds[1], "OK", 2);
+    if (pid < 0)
+        err(EXIT_FAILURE, "Failed to create container process");
+    int res = waitpid(pid, NULL, 0);
+    if (res == -1)
+        err(EXIT_FAILURE, "Waitpid failed");
+    printf("Child terminated\n");
+    return 0;
 }
